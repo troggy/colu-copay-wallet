ebbe1299ddf777b16ed2ef56debf957d10486e4a
Kosta Korenkov <7r0ggy@gmail.com>
2015-11-16 22:27:12 +0300

    Store default wallet asset on wallet creation

--- src/js/services/profileService.js
+++ src/js/services/profileService.js
@@ -250,7 +250,8 @@ angular.module('copayApp.services')
         var walletName = gettextCatalog.getString('Personal Wallet');
         var me = gettextCatalog.getString('me');
         walletClient.createWallet(walletName, me, 1, 1, {
-          network: 'livenet'
+          network: 'livenet',
+          customData: opts.customData
         }, function(err) {
           if (err) return bwsError.cb(err, gettext('Error creating wallet'), cb);
           var p = Profile.create({
@@ -267,7 +268,8 @@ angular.module('copayApp.services')
         if (err) return cb(err);
 
         walletClient.createWallet(opts.name, opts.myName || 'me', opts.m, opts.n, {
-          network: opts.networkName
+          network: opts.networkName,
+          customData: opts.customData
         }, function(err, secret) {
           if (err) return bwsError.cb(err, gettext('Error creating wallet'), cb);
 
@@ -527,6 +529,9 @@ angular.module('copayApp.services')
       configService.get(function(err) {
         bwcService.setBaseUrl(defaults.bws.url);
         bwcService.setTransports(['polling']);
+        opts.customData = {
+          walletAsset: defaults.assets.defaultAsset
+        };
         root._createNewProfile(opts, function(err, p) {
           if (err) return cb(err);
 
