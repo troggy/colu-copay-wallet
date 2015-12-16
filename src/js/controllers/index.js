39f34409a5053f06c7d7d1472b90ded313513ff0
Kosta Korenkov <7r0ggy@gmail.com>
2015-12-16 23:14:28 +0300

    Fix previous amount shown before assets received

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -379,7 +379,6 @@ angular.module('copayApp.controllers').controller('indexController', function($r
           self.totalAssetBalanceStr = walletService.totalAssetBalanceStr;
           self.walletUnit = walletService.walletUnit;
         }
-        updateAssetBalance();
 
         $rootScope.$on('ColoredCoins/AssetsUpdated', updateAssetBalance);
         $rootScope.$on('Local/WalletAssetUpdated', updateAssetBalance);
