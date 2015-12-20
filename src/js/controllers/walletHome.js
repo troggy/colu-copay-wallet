009582fc594f2e5cf669c18cafeab56eab753f8b
Kosta Korenkov <7r0ggy@gmail.com>
2015-12-20 14:28:35 +0300

    Always show asset details for colored proposals
    (even if wallet is BTCish)

--- src/js/controllers/walletHome.js
+++ src/js/controllers/walletHome.js
@@ -274,7 +274,7 @@ angular.module('copayApp.controllers').controller('walletHomeController',
       $scope.loading = null;
       $scope.color = fc.backgroundColor;
       $scope.isShared = fc.credentials.n > 1;
-      $scope.isAsset = walletService.walletAsset.isAsset;
+      $scope.isAsset = tx.customData && tx.customData.asset;
 
       // ToDo: use tx.customData instead of tx.message
       if (tx.message === 'Glidera transaction' && isGlidera) {
