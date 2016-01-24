b1b40ac54719099e5a943911aab25fcd530c8ecc
Kosta Korenkov <7r0ggy@gmail.com>
2016-01-24 17:51:47 +0300

    Fix progress indicator

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -69,6 +69,9 @@ angular.module('copayApp.controllers').controller('indexController', function($r
     });
     // The first one
     self.onGoingProcessName = name || processName;
+    if (isOn !== undefined) {
+      self.onGoingProcessName = null;
+    }
     $timeout(function() {
       $rootScope.$apply();
     });
