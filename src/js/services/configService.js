ffd779ac63dd402789b77fbb7468606abffff015
Kosta Korenkov <7r0ggy@gmail.com>
2016-01-28 11:04:13 +0300

    Use domain name and HTTPS for BWS

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -12,7 +12,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
     // Bitcore wallet service URL
     bws: {
-      url: 'http://127.0.0.1:3232/bws/api',
+      url: 'https://bws.urv2.com.br/bws/api',
     },
 
     // wallet default config
