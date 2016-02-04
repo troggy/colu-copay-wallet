aa9308465819924b5551200b848139ad08d9f63c
Kosta Korenkov <7r0ggy@gmail.com>
2016-02-04 13:38:27 +0300

    Log to Loggly only errors

--- src/js/routes.js
+++ src/js/routes.js
@@ -22,6 +22,7 @@ angular
     
     if (loggly.enabled) {
       LogglyLoggerProvider
+        .level('ERROR')
         .inputToken(loggly.token)
         .inputTag(loggly.env)
         .sendConsoleErrors(true);
