58af65aa8f214c7332987700523dab19437cdf10
Kosta Korenkov <7r0ggy@gmail.com>
2016-02-09 13:18:47 +0300

    Move instance config to separate file to ease configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+window.unicoisaConfig = { 
+  walletName: "Bitreals",
+  color: "#2980b9",
+  asset: {
+    assetId: "LFu6pNp5FLHQu1RERkYEjPjxFZLD3zNJAbhYz",
+    name: "Bitreal",
+    symbol: "R$",
+    pluralSymbol: "R$"
+  }
+};
