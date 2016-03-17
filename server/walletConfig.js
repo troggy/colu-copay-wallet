285e23dd9a9acff0e85185c700fad16e6b234a44
Kosta Korenkov <7r0ggy@gmail.com>
2016-03-17 14:06:30 +0300

    Handle defaults

--- server/walletConfig.js
+++ server/walletConfig.js
@@ -1,22 +1,23 @@
 'use strict';
 
 module.exports = function(walletData) {
+
   return `
 window.unicoisaConfig = {
   walletName: "${walletData.walletName}",
-  mainColor: "${walletData.mainColor}",
-  secondaryColor: "${walletData.secondaryColor}",
+  mainColor: "${walletData.mainColor || ''}",
+  secondaryColor: "${walletData.secondaryColor || ''}",
   assets: [
     {
       assetId: '${walletData.assetId}',
       name: '${walletData.assetName}',
-      symbol: '${walletData.symbol}',
-      pluralSymbol: '${walletData.pluralSymbol}'
+      symbol: '${walletData.symbol || ''}',
+      pluralSymbol: '${walletData.pluralSymbol || ''}'
     }
   ],
   defaultAsset: '${walletData.assetId}',
-  logo: "${walletData.logo}",
-  coluApiKey: "${walletData.coluApiKey}"
+  logo: "${walletData.logo || ''}",
+  coluApiKey: "${walletData.coluApiKey || ''}"
 };
   `
 };
