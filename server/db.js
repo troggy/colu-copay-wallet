90ad8c630706aec0bd78b8670692a72f1943946b
Kosta Korenkov <7r0ggy@gmail.com>
2016-03-17 08:03:54 +0300

    Serve wallet config from database

---
+++ server/db.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var MongoClient = require('mongodb');
+
+let getWallet = function(dbUrl, walletName) {
+    return new Promise(function(resolve, reject) {
+        MongoClient.connect(dbUrl, function(err, db) {
+          db.collection('wallets')
+            .find({ walletName: walletName })
+            .next((err, wallet) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(wallet);
+              }
+            });
+        });
+    });
+};
+
+module.exports = function(dbUrl) {
+    return {
+      getWallet: function(walletName) { return getWallet(dbUrl, walletName); }
+    }
+};
