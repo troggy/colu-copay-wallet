b15fbf3ee0f2373278e640f3b4e9e4c563a5d266
Kosta Korenkov <7r0ggy@gmail.com>
2016-03-20 14:47:04 +0300

    Use config file hosted on other server

--- src/js/services/instanceConfig.js
+++ src/js/services/instanceConfig.js
@@ -1,3 +1,20 @@
 'use strict';
 
-angular.module('copayApp').constant('instanceConfig', unicoisaConfig);  
+angular.module('copayApp.services').provider('instanceConfig', function() {
+  var provider = {};
+
+  if (window.unicoisaConfig) {
+    provider.config = window.unicoisaConfig;
+  } else {
+    console.error('No wallet config found');
+    provider.config = {
+      walletName: 'Wallet not found'
+    };
+  }
+
+  provider.$get = function() {
+    return provider.config;
+  }
+
+  return provider;
+});
