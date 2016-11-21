c0babb36023cad02d78bea22f344fba7fecfc6b3
Kosta Korenkov <kosta@whoot.me>
2016-11-21 23:25:46 +0300

    feat: use coloredcoins logo

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -317,9 +317,25 @@ angular.module('copayApp.directives')
       },
       controller: function($scope, instanceConfig) {
         $scope.logo_url = instanceConfig.logo || 'img/logo-negative.svg';
+        if ($scope.width) {
+          var logo_width = $scope.width * 1.5;
+          var logo_height = logo_width / (220 / 43);
+          $scope.copay_logo_style = 'width: ' + $scope.width + 'px;';
+          $scope.logo_style = "background-size: " + logo_width + "px " + logo_height + "px;" +
+              "width: " + logo_width + "px; height: " + logo_height + "px;";
+        } else {
+          var copay_width = 100 / 1.5 + '%';
+          $scope.copay_logo_style = 'width: ' + copay_width + '%; max-width: 147px';
+          $scope.logo_style = "background-size: 100% auto; width: 100%; max-width: 220px; height: 43px;";
+        }
       },
       replace: true,
-      template: '<img ng-src="{{ logo_url }}" alt="Copay">'
+      template: '' +
+          '<div class="cc-logo-holder" ng-class="{ \'negative\' : negative, \'inline\' : width < 50, }">' +
+            '<img ng-src="{{ logo_url }}" alt="Copay" style="{{ copay_logo_style }}">' +
+            '<div class="cc-plus">+</div>' +
+            '<div class="cc-logo" style="{{ logo_style }}"></div>' +
+          '</div>'
     }
   })
   .directive('availableBalance', function() {
